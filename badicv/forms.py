49f592cfa584468e35f436e6ab34d50f1b863955
Badi James <badi.t.james@gmail.com>
2018-02-11 15:15:03 +1300

    Search css. Made 'sticky but not fixed' bottom bar

--- badicv/forms.py
+++ badicv/forms.py
@@ -8,8 +8,7 @@ from . import models
 
 class ExperienceSearchForm(forms.Form):
     
-    search_term = forms.CharField(label='search term', max_length=128, 
-                                  required=False)
+    search_term = forms.CharField(label='search', required=False)
     choices = [('','')]
     choices.extend(models.Experience.type_choice)
     type = forms.ChoiceField(choices=tuple(choices), required=False)
@@ -17,8 +16,7 @@ class ExperienceSearchForm(forms.Form):
 
 class SkillSearchForm(forms.Form):
     
-    search_term = forms.CharField(label='search term', max_length=128,
-                                  required=False)
+    search_term = forms.CharField(label='search', required=False)
     choices = [('','')]
     choices.extend([(sk.skill_type, sk.skill_type) for sk in models.SkillType.objects.all()])
     type = forms.ChoiceField(choices=tuple(choices), required=False)
